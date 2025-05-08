fd5839bf8f398fc1fea9e309adfb79ec12aa1870
yeshey <yesheytenzin0@gmail.com>
2025-05-08 23:14:07 +0600

    1

--- talkar.c
+++ talkar.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main() {
-  int a;
-  int b;
-  scanf("%d\n",&a);
-  scanf("%d\n",&b);
-  if(a>b){
-    printf("MAGA!");
-  }elseif(a<b){
-    printf("FAKE NEWS!");
-  }else{
-    printf("WORLD WAR 3!");
-  }
+    int a;
+    int b;
+    scanf("%d\n",&a);
+    scanf("%d\n",&b);
+    if(a>b){
+        printf("MAGA!");
+    }else if(a<b){
+        printf("FAKE NEWS!");
+    }else{
+        printf("WORLD WAR 3!");
+    }
 }
